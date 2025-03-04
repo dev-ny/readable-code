15cdced3c2891aa268e6cdb482ee9815d95f3b78
devny <ceoahn95@gmail.com>
2025-03-04 16:36:37 +0900

    2025 03 04 by devny - 사용하는 곳 가까운 곳에서 변수를 선언하자!
    - while 문 안에서 스캐너가 계속 생성되기 때문에 상수로 선언해 줌.

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -9,6 +9,7 @@ public class MinesweeperGame {
     // 중요한 문자열, 숫자야. 유지보수할때 잘 봐야해! 할 수 있는 것 = 매직넘버, 매직스트링
     public static final int BOARD_ROW_SIZE = 8;
     public static final int BOARD_COL_SIZE = 10;
+    public static final Scanner SCANNER = new Scanner(System.in);
     // 상수 컨벤션 = 대문자와 언더스코어로 이루어져 있게 해야함
     private static final String[][] BOARD = new String[BOARD_ROW_SIZE][BOARD_COL_SIZE];
     private static final Integer[][] NEAR_BY_LAND_MINE_COUNTS = new Integer[BOARD_ROW_SIZE][BOARD_COL_SIZE];
@@ -23,7 +24,6 @@ public class MinesweeperGame {
 
     public static void main(String[] args) {
         showGameStartCommand();
-        Scanner scanner = new Scanner(System.in);
         initializeGame();
         while (true) {
             showBoard();
@@ -36,8 +36,8 @@ public class MinesweeperGame {
                 break;
             }
             System.out.println();
-            String cellInput = getCellInputFromUser(scanner);
-            String userActionInput = getUserActionInputFromUser(scanner);
+            String cellInput = getCellInputFromUser();
+            String userActionInput = getUserActionInputFromUser();
 
             actOnCell(cellInput, userActionInput);
         }
@@ -95,15 +95,15 @@ public class MinesweeperGame {
         return convertColFrom(cellInputCol);
     }
 
-    private static String getUserActionInputFromUser(Scanner scanner) {
+    private static String getUserActionInputFromUser() {
         System.out.println("선택한 셀에 대한 행위를 선택하세요. (1: 오픈, 2: 깃발 꽂기)");
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 
-    private static String getCellInputFromUser(Scanner scanner) {
+    private static String getCellInputFromUser() {
         System.out.println("선택할 좌표를 입력하세요. (예: a1)");
 
-        return scanner.nextLine();
+        return SCANNER.nextLine();
     }
 
     private static boolean doesUserLoseTheGame() {
