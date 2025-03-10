8864b7428d72e12adc4c55f9f4c68d40416bdc13
devny <ceoahn95@gmail.com>
2025-03-10 15:11:14 +0900

    2025 03 10 by devny - ISP(인터페이스 분리 원칙)_2 - AnotherGame 추가(initialize 가 필요 없음)   + 이 경우 Game클래스에서 initialize 선언부가 변경되면 AnotherGame 클래스에서도 변경을 해주어야 됨. 같이 수정이 되어야 하기 때문에 문제 점이 있음. -> ISP 원칙에 위배!

---
+++ src/main/java/cleancode/minesweeper/tobe/AnotherGame.java
@@ -0,0 +1,13 @@
+package cleancode.minesweeper.tobe;
+
+public class AnotherGame implements Game {
+    @Override
+    public void initialize() {
+        // ...필요없는데...
+    }
+
+    @Override
+    public void run() {
+        // 게임 진행
+    }
+}
